1fd76eff109abd0e9e69ba5b6f3c9304f80fe39e
Seungho Lee <lee.seungho.001@gmail.com>
2021-07-24 23:28:00 +0900

    error fixed

--- tb/tb_Bottleneck_Layer.v
+++ tb/tb_Bottleneck_Layer.v
@@ -16,9 +16,10 @@
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//  Simulation of receiving and processing data from the encoder layer.
+//  Depending on the handshake signal 'request_next_iact', 
+//  it is decided whether to receive or hold data.
 //////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_Bottleneck_Layer;
     localparam DATA_BITWIDTH = 8;
@@ -33,28 +34,25 @@ module tb_Bottleneck_Layer;
     localparam WEIGHTS_ADDR_BITWIDTH = $clog2(DILATED_KW * DILATED_KH) + 1;
     localparam IACTS_ADDR_BITWIDTH = 5;
     reg clk, rstN;
-    wire[DATA_BITWIDTH * NUM_OF_WEIGHT-1:0] w_oacts; // 레이어 출력 포트
+    wire request_next_iact;
+    wire[DATA_BITWIDTH * NUM_OF_WEIGHT-1:0] w_oacts; // output port of bottleneck layer
 
+    // these registers are registering the result of S/W reference code and can be compared with verilog's output.
     reg[DATA_BITWIDTH-1:0] iact_register[0:9*NUM_OF_CHANNEL-1];
     reg[DATA_BITWIDTH-1:0] oact_register[0:9*NUM_OF_CHANNEL-1];
     reg[DATA_BITWIDTH-1:0] wght_register[0:NUM_OF_WEIGHT*DILATED_KW*DILATED_KW*NUM_OF_CHANNEL-1];
 
     wire[NUM_OF_CHANNEL*DATA_BITWIDTH-1:0] iact_concat_by_chan[0:8];
     wire[NUM_OF_WEIGHT*NUM_OF_CHANNEL*DATA_BITWIDTH-1:0] wght_concat_by_chan[0:48];
-
+    wire[DATA_BITWIDTH-1:0] divided_oacts[0:NUM_OF_WEIGHT - 1];
     reg[NUM_OF_CHANNEL*DATA_BITWIDTH-1:0] iacts; 
     reg[NUM_OF_WEIGHT*NUM_OF_CHANNEL*DATA_BITWIDTH-1:0] wghts; 
-    reg[DATA_BITWIDTH-1:0] oacts[0:9*NUM_OF_CHANNEL-1]; // 결과 저장
+    reg[DATA_BITWIDTH-1:0] oacts[0:9*NUM_OF_CHANNEL-1]; 
 
     genvar gi, gj, gk;
 
     generate
         for(gi = 0; gi < NUM_OF_WEIGHT; gi = gi + 1) begin
-            0 <= 32*8-1:31*8 
-            1 <= 31*8-1:30*8
-            ...
-            31 <= 1*8-1:0*8
-            always @(*) r
             assign divided_oacts[gi] = w_oacts[gi * DATA_BITWIDTH +: DATA_BITWIDTH];
         end
     endgenerate
@@ -85,32 +83,39 @@ module tb_Bottleneck_Layer;
                             .clk(clk), .rstN(rstN), 
                             .iacts(iacts), 
                             .wghts(wghts),
+                            .request_next_iact(request_next_iact),
         	                .oacts(w_oacts) );
 
     always #10 clk = ~clk;
 
     initial begin
-        $readmemh("C:/Workspace/CIS/bottleneck/bottleneck_layer/hex/[3x3x32] aspp_input_h.mem", iact_register);
-        $readmemh("C:/Workspace/CIS/bottleneck/bottleneck_layer/hex/[3x3x32] aspp_output_h.mem", oact_register);
-        $readmemh("C:/Workspace/CIS/bottleneck/bottleneck_layer/hex/[32x7x7x32] aspp3_w_h_zeros.mem", wght_register);
+        $readmemh("/home/s-lee/CIS/repos/DLFI-HW-ACC/dat_aspp/[3x3x32] aspp_input_1_h.mem", iact_register);
+        $readmemh("/home/s-lee/CIS/repos/DLFI-HW-ACC/dat_aspp/[3x3x32] aspp_output_h.mem", oact_register);
+        $readmemh("/home/s-lee/CIS/repos/DLFI-HW-ACC/dat_aspp/[32x7x7x32] aspp3_w_h_zeros.mem", wght_register);
         // $stop;
         clk = 0; rstN = 1; 
         #10 rstN = 0;
         #10 rstN = 1; 
     end
 
-
     integer i;
     initial begin
         for(i = 0; i < 9; i = i + 1) begin
-            #20 iacts = iact_concat_by_chan[i]; wghts = wght_concat_by_chan[i];
-            wghts = wght_concat_by_chan[i];
+            #20 iacts = iact_concat_by_chan[i];
         end
-        for(i = 9; i < 49; i = i + 1) begin
-            #20 wghts = wght_concat_by_chan[i];
+        
+        #200 $readmemh("/home/s-lee/CIS/repos/DLFI-HW-ACC/dat_aspp/[3x3x32] aspp_input_2_h.mem", iact_register);
+        for(i = 0; i < 9; i = i + 1) begin
+            #20 iacts = iact_concat_by_chan[i];
         end
     end
 
-
+    integer j;
+    initial begin
+        for(j = 9; j < 49; j = j + 1) begin
+            #20 wghts = wght_concat_by_chan[j];
+        end
+    end
+    
 
 endmodule
